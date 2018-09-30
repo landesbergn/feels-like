3cead6261516767b60167d9699b8a11d815dea3a
landesbergn <noah.landesberg@gmail.com>
2018-09-30 20:34:55 +0000

    Be more smart with error messages. Also don't load label presets.

--- Feels Like/ViewController.swift
+++ Feels Like/ViewController.swift
@@ -17,12 +17,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
     @IBOutlet weak var feelsLikeTmpLbl: UILabel!
     @IBOutlet weak var summaryLbl: UILabel!
     @IBOutlet weak var asOfLbl: UILabel!
+    @IBOutlet weak var errorLabel: UILabel!
     
     // Used to start getting the users location
     let locationManager = CLLocationManager()
     
     // Load the DarkSkyClient from the ForecastIO package
-    let client = DarkSkyClient(apiKey: "")
+    let client = DarkSkyClient(apiKey: "41485dc0bc40c267ad42c49793e351e9")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +39,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
         // Set DarkSky defaults as US and English
         client.units = .us
         client.language = .english
+        
+        label.text = ""
+        cityLbl.text = ""
+        summaryLbl.text = ""
+        errorLabel.text = ""
+        
+        // set in the UI directly
+        // feelsLikeTmpLbl.text = "--"
+        // asOfLbl.text = "last updated: "
 
     }
     
@@ -58,7 +68,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
             self.asOfLbl.text = "last updated: " + date_string
             print(date_string)
             if (error != nil) {
-                print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
+                self.inCaseOfError(errorClass: "Geocoder", errorString: (error?.localizedDescription)!)
                 return
             }
             
@@ -67,14 +77,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
                 self.displayLocationInfo(pm)
                 print (self.displayLocationInfo(pm))
             } else {
-                print("Problem with the data received from geocoder")
+                self.inCaseOfError(errorClass: "Geocoder", errorString: "Problem with the data received from geocoder")
             }
             
             let location = locations.first
-            // if I want to force stop location updates, use below
+            // If I want to force stop location updates, use below:
             // self.locationManager.stopUpdatingLocation()
             self.updateWeatherForLocation(lat: (location?.coordinate.latitude)!, lon: (location?.coordinate.longitude)!)
-            
         })
     }
     
@@ -82,13 +91,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
         self.client.getForecast(latitude: lat, longitude: lon, excludeFields: [.alerts, .daily, .flags, .minutely]) { result in
             switch result {
             case .success(let currentForecast, let requestMetadata):
-                //  We got the current forecast!
+                // We got the current forecast!
+                // Get the 'Feels Like' temperature from result and update label in UI
+                // (this is where the magic happens!)
                 if let currentFeelsTemperature: Double = currentForecast.currently?.temperature {
                     print(currentFeelsTemperature)
                     DispatchQueue.main.async {
                         self.feelsLikeTmpLbl.text = String(Int(round(currentFeelsTemperature)))
                     }
                 }
+                // Get the summary string from result and update label in UI
                 if let summaryInfo: String = currentForecast.currently?.summary {
                     print(summaryInfo)
                     DispatchQueue.main.async {
@@ -98,7 +110,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
                 print(requestMetadata)
             case .failure(let error):
                 //  Uh-oh. We have an error!
-                print(error)
+                self.inCaseOfError(errorClass: "API Call", errorString: error as! String)
             }
         }
     }
@@ -118,17 +130,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
         }
         
         func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-            print("Error while updating location " + error.localizedDescription)
+            //  Uh-oh. We have an error!
+            self.inCaseOfError(errorClass: "Geocoder", errorString: String("Error while updating location " + error.localizedDescription))
         }
         
+    }
+    
+    func inCaseOfError(errorClass:String, errorString:String) {
+        if (errorClass == "Geocoder") {
+            errorLabel.text = "Error retriving location"
+        } else if (errorClass ==  "API Call") {
+            errorLabel.text = "Error retriving weather information"
+        } else {
+            errorLabel.text = "You found a new kind of error. Cool!"
+        }
+        print("[" + errorClass + "] " + errorString)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
 
 }
 
