cbec2a2ae30af58a88e4c5d7c89114776aea9d46
landesbergn <noah.landesberg@gmail.com>
2018-12-30 18:23:10 -0500

    use plist file to store/hide api key

--- Feels Like/ViewController.swift
+++ Feels Like/ViewController.swift
@@ -85,7 +85,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
     let locationManager = CLLocationManager()
     
     // Load the DarkSkyClient from the ForecastIO package
-    let client = DarkSkyClient(apiKey: "41485dc0bc40c267ad42c49793e351e9")
+    let client = DarkSkyClient(apiKey: NSDictionary(contentsOfFile: Bundle.main.path(forResource: "keys", ofType: "plist")!)!.object(forKey: "darkSkyKey") as! String)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -348,4 +348,3 @@ class ViewController: UIViewController, CLLocationManagerDelegate  {
     }
     
 }
-
